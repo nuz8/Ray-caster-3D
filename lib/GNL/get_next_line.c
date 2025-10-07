81a9c0b2d37177038087a2f11f73f5c211ff555f
Samed Demiroz <sdemiroz@2-A-1.42heilbronn.de>
2025-10-07 09:09:39 +0200

    fixed one last norminette error

--- lib/GNL/get_next_line.c
+++ lib/GNL/get_next_line.c
@@ -11,136 +11,75 @@
 /* ************************************************************************** */
 
 #include "../includes/get_next_line.h"
-#include <sys/mman.h>
 #include <unistd.h>
 
-char	*get_next_line(int fd);
-size_t	line_length(char *str);
-char	*join_parts(char *s1, char *s2);
-char	*copy_n_shift(char *buffer);
-char	*extract_rest(char *next_line, char *buffer, int fd);
+#define MAX_FD 4096
+
+char		*get_next_line(int fd);
+static char	*ensure_data(int fd, char buffer[][BUFFER_SIZE + 1]);
+static char	*extend_line(int fd, char *line, char buffer[][BUFFER_SIZE + 1]);
+static char	*grow_line(int fd, char *line, char buffer[][BUFFER_SIZE + 1]);
 
 char	*get_next_line(int fd)
 {
-	ssize_t	bytes_read;
-	char	*next_line;
-	char	*joined_line;
+	static char	buffer[MAX_FD][BUFFER_SIZE + 1];
+	char		*line;
 
-	static char(*buffer)[BUFFER_SIZE + 1] = NULL;
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!buffer)
-		buffer = mmap(NULL, sizeof(char) * 4096 * (BUFFER_SIZE + 1),
-				PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-	if (buffer == MAP_FAILED)
+	line = ensure_data(fd, buffer);
+	if (!line)
 		return (NULL);
+	if (line[line_length(line) - 1] != '\n')
+		line = extend_line(fd, line, buffer);
+	return (line);
+}
+
+static char	*ensure_data(int fd, char buffer[][BUFFER_SIZE + 1])
+{
+	ssize_t	bytes_read;
+
 	if (!buffer[fd][0])
 	{
-		bytes_read = read(fd, &(buffer[fd][0]), BUFFER_SIZE);
+		bytes_read = read(fd, buffer[fd], BUFFER_SIZE);
 		if (bytes_read <= 0)
 			return (NULL);
 		buffer[fd][bytes_read] = '\0';
 	}
-	next_line = copy_n_shift(&(buffer[fd][0]));
-	if (!next_line)
-		return (NULL);
-	if (next_line[line_length(next_line) - 1] != '\n')
-	{
-		joined_line = extract_rest(next_line, &(buffer[fd][0]), fd);
-		if (!joined_line)
-			return (NULL);
-		next_line = joined_line;
-	}
-	return (next_line);
+	return (copy_n_shift(buffer[fd]));
 }
 
-size_t	line_length(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (*str && *str != '\n')
-	{
-		i++;
-		str++;
-	}
-	if (*str == '\n')
-		i++;
-	return (i);
-}
-
-char	*join_parts(char *s1, char *s2)
-{
-	char	*joined_str;
-	char	*str_ptr[2];
-	size_t	str_len[2];
-	size_t	len;
-	size_t	i;
-
-	str_len[0] = line_length(s1);
-	str_len[1] = line_length(s2);
-	len = str_len[0] + str_len[1];
-	joined_str = (char *)malloc((len + 1) * sizeof(char));
-	if (!joined_str)
-		return (NULL);
-	i = 0;
-	str_ptr[0] = s1;
-	str_ptr[1] = s2;
-	while (i < str_len[0])
-		joined_str[i++] = *s1++;
-	while (i >= str_len[0] && i < len)
-		joined_str[i++] = *s2++;
-	joined_str[i] = '\0';
-	return (free(str_ptr[0]), free(str_ptr[1]), joined_str);
-}
-
-char	*copy_n_shift(char *buffer)
-{
-	char	*line_part;
-	char	*buf_ptr;
-	int		index;
-	int		line_len;
-
-	if (!buffer)
-		return (NULL);
-	line_len = line_length(buffer);
-	line_part = (char *)malloc((line_len + 1) * sizeof(char));
-	if (!line_part)
-		return (NULL);
-	index = 0;
-	buf_ptr = buffer;
-	while (*buffer && index < line_len)
-		line_part[index++] = *buffer++;
-	line_part[index] = '\0';
-	while (*buffer)
-		*buf_ptr++ = *buffer++;
-	*buf_ptr = *buffer;
-	return (line_part);
-}
-
-char	*extract_rest(char *next_line, char *buffer, int fd)
+static char	*extend_line(int fd, char *line, char buffer[][BUFFER_SIZE + 1])
 {
 	ssize_t	bytes_read;
-	char	*line_part;
-	char	*joined_line;
 
-	while (next_line[line_length(next_line) - 1] != '\n')
+	while (line && line[line_length(line) - 1] != '\n')
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read < 0)
-			return (free(next_line), NULL);
-		else if (bytes_read == 0)
-			return (next_line);
-		buffer[bytes_read] = '\0';
-		line_part = copy_n_shift(buffer);
-		if (!line_part)
-			return (free(next_line), NULL);
-		joined_line = join_parts(next_line, line_part);
-		if (!joined_line)
-			return (free(next_line), free(line_part), NULL);
-		next_line = joined_line;
+		bytes_read = read(fd, buffer[fd], BUFFER_SIZE);
+		if (bytes_read <= 0)
+		{
+			if (bytes_read < 0)
+				return (free(line), NULL);
+			return (line);
+		}
+		buffer[fd][bytes_read] = '\0';
+		line = grow_line(fd, line, buffer);
+		if (!line)
+			return (NULL);
 	}
-	return (next_line);
+	return (line);
+}
+
+static char	*grow_line(int fd, char *line, char buffer[][BUFFER_SIZE + 1])
+{
+	char	*part;
+	char	*joined;
+
+	part = copy_n_shift(buffer[fd]);
+	if (!part)
+		return (free(line), NULL);
+	joined = join_parts(line, part);
+	if (!joined)
+		return (free(line), free(part), NULL);
+	return (joined);
 }
